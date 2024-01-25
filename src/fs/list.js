1ec34a6c711a4cdee91737a97a23dd5ff90c77a0
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2024-01-26 02:05:34 +0500

    feat: solve fs block

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filesToList = join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
-};
+  try {
+    const files = await readdir(filesToList);
+    for (const file of files) {
+      console.log(file);
+    }
+  } catch {
+      throw new Error('FS operation failed'); 
+    }
+}
 
 await list();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import * as fs from "node:fs/promises";
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToRename = join(__dirname, 'files', 'wrongFilename.txt');
+const properFileName = join(__dirname, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fs.rename(fileToRename, properFileName);
+  } catch {
+      throw new Error('FS operation failed');
+    } 
 };
 
 await rename();
