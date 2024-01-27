847448caa0355a072725b9142a25ecca19f19a33
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2024-01-27 19:00:14 +0500

    feat: solve streams block

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import { createReadStream } from 'node:fs';
+import { Transform } from 'stream';
+import process from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filenamePath = join(__dirname, 'files', 'fileToRead.txt');
+
 const transform = async () => {
-    // Write your code here 
+  const fileToRead = createReadStream(filenamePath);
+
+  const reverse = new Transform({
+   transform(chunk, _, callback) {
+    callback(null, chunk.toString('utf-8').split('').reverse().join(''))
+   }
+  });
+
+  fileToRead.pipe(reverse).pipe(process.stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import { createWriteStream } from 'node:fs';
+import process from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filenamePath = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const fileToWrite = createWriteStream(filenamePath, {flags: 'a'});
+  process.stdin.pipe(fileToWrite);
+  console.log('Type some text. Press Enter first, then Ctrl + C to exit'); 
 };
 
 await write();
