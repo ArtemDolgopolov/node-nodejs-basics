4171f797340b2efcb31d05e8e206483a729b38af
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2024-01-27 15:55:18 +0500

    feat: solve hash block

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createReadStream } from 'node:fs';
+const { createHash } = await import('node:crypto');
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filename = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+ const hash = createHash('sha256');
+ const input = createReadStream(filename);
+ 
+ input.on('readable', () => {
+   const data = input.read();
+   if (data)
+     hash.update(data);
+   else {
+     console.log(`The hash is: ${hash.digest('hex')}`);
+   }
+ });
 };
 
 await calculateHash();
